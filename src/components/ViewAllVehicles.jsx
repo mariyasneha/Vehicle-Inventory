6e2eea1875a3025c77357d2572c05e126718eb3a
mariyasneha <mariyasneha104@gmail.com>
2025-07-13 19:55:50 +0530

    Second Commit

--- src/components/ViewAllVehicles.jsx
+++ src/components/ViewAllVehicles.jsx
@@ -13,7 +13,7 @@ const ViewAllVehicles = () => {
                 <VehicleNavBar />
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                        <table class="table">
+                        <table class="table table-bordered">
                             <thead>
                                 <tr>
                                     <th scope="col">Id</th>
